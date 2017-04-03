11e12c9f270d870914fadaec6ff8ce7cc06f8260
Rob Dumas <robdumas@gmail.com>
2017-04-02 21:19:29 -0500

    Removed line for bash_completion

--- osx/defaults.sh
+++ osx/defaults.sh
@@ -2,4 +2,4 @@
 
 test -e "${HOME}/.iterm2_shell_integration.bash" && source "${HOME}/.iterm2_shell_integration.bash"
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
-test -e "$(brew --prefix)/etc/bash_completion" && source "$(brew --prefix)/etc/bash_completion"
+# test -e "$(brew --prefix)/etc/bash_completion" && source "$(brew --prefix)/etc/bash_completion"
