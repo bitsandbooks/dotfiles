cf0c572e8ffb0c8a8d7880b50b8b94b5e7d995ad
Rob Dumas <robdumas@gmail.com>
2018-09-01 11:45:23 -0500

    Changes LESS options; added `bootplz` alias

--- sh/vars.sh
+++ sh/vars.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
-export PROJECTS="$HOME/projects"
 export EDITOR="vim"
+export LESS="-FiJMNqsXx8"
+export PATH="$HOME/bin:$HOME/.local/bin:/usr/local/bin:/opt/bin:/usr/bin:/bin:/usr/sbin:/sbin:."
+export PROJECTS="$HOME/projects"
 export VISUAL="$EDITOR"
 export VIM="$HOME/.dotfiles/vim"
-export PATH="$HOME/bin:$HOME/.local/bin:/usr/local/bin:/opt/bin:/usr/bin:/bin:/usr/sbin:/sbin:."
 
 # history file
 export HISTSIZE=1000 # Size of history (default = 500)
 export HISTFILESIZE=10000
 export HISTCONTROL="erasedups" # Ignore duplicates
 export HISTIGNORE="ll:ls:ls *:cd:cd -:pwd:exit:date:* --help" # Commands to ignore
-
-# Don’t clear the screen after quitting a manual page
-export MANPAGER="less -X"
