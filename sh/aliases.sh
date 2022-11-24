ca2f8e1540bdffb000b68a832fe2c4258305727c
Rob Dumas <robdumas@gmail.com>
2022-11-23 19:32:05 -0600

    One bug and a few minor changes
    - **zsh/zshenv.symlink:** Upped Zsh history file size from 500 to 50000.
    - **zsh/zshrc.symlink:** Clearer comment for .localrc
    - **sh/aliases.sh:** Using clearer aliases. Also fixed bug with “path” alias. (A variable inside quotes must be prefaced with \ to avoid the var being assessed only on shell start.)

--- sh/aliases.sh
+++ sh/aliases.sh
@@ -7,13 +7,11 @@ alias ...='cd ../..'
 # Other simple aliases
 alias cls=clear
 alias hist='fc -l 1'
-alias ll='ls -lah'
-alias lll='ls -lah | less'
-alias lsd='ls -lah | grep "^d"' # List only directories
-alias lld=lsd
-alias moar='less -X' # for the lulz
+alias ll='ls --almost-all --color=auto --human-readable -l -v'
+alias lll='ll | less'
+alias lld='ll | grep "^d"' # List only directories
 alias sl=ls # I still do this ALL the time
-alias path="echo $PATH | tr ':' '\n'"
+alias path="echo \$PATH | tr ':' '\n'"
 
 # Aliases related to apps
 alias curl='curl --progress-bar'
