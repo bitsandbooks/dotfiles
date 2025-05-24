6c27bbcaab38edc959ef8cd4f8f8affc46981222
Rob Dumas <robdumas@gmail.com>
2025-05-24 13:29:33 -0500

    Brewfile for Homebrew packages and updates
    - **macos/Brewfile:** contains Homebrew taps, formulae, and casks to install.
    - **macos/vars.sh:** added new variable, `$DOTFILES`.
    - **macos/aliases.sh:** changed `brewup` alias to use Brewfile in the same folder.

--- macos/aliases.sh
+++ macos/aliases.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 alias alint=ansible-lint
 alias aplay=ansible-playbook
-alias brewup='brew update && brew upgrade && brew cleanup'
+alias brewup="brew bundle --upgrade --file $DOTFILES/macos/Brewfile"
 alias o=open
 alias oo='open .'
 alias ls='ls -GH'
--- macos/vars.sh
+++ macos/vars.sh
@@ -3,3 +3,4 @@
 export ANSIBLE_CONFIG=~/.ansible.cfg
 export BYOBU_PREFIX=/usr/local
 export PATH="/Library/Developer/CommandLineTools/usr/bin:$PATH"
+export DOTFILES="$HOME/.dotfiles"
