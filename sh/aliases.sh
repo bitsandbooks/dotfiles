91caf99a3eca8eef4d98972bfa9960cc4e65d266
Rob Dumas <robdumas@gmail.com>
2025-05-24 14:23:48 -0500

    sh/aliases cleanup
    Using [oh-my-zsh](https://ohmyz.sh/) on the new laptop, so it’s time to clean up a few options in `sh/aliases.sh`:
    
    - removed `..` and `…` aliases, which were infrequently used and are covered by OMZ anyway
    - changed `ll` alias to use abbreviated options, since double-dash CLI options are not present on all platforms; single-dash options are more consistent
    - `curl` and `wget` aliases now call ull binary
    - removed `py2`

--- sh/aliases.sh
+++ sh/aliases.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-# Easier navigation
-alias ..='cd ..'
-alias ...='cd ../..'
-
 # Other simple aliases
 alias cls=clear
 alias hist='fc -l 1'
-alias ll='ls --almost-all --color=auto --human-readable -l -v'
+alias ll="ls -Ahl --color=auto"
 alias lll='ll | less'
 alias lld='ll | grep "^d"' # List only directories
 alias sl=ls # I still do this ALL the time
