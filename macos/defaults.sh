6390569abdc4cdc86f6098851b1db968ce32e756
Rob Dumas <robdumas@gmail.com>
2025-05-24 23:45:52 -0500

    macOS cleanup
    - **macos/defaults.sh:** We don’t need the iterm shell integration right now.
    - **macos/vars.sh:** Removed byobu prefix; updated ANSIBLE_CONFIG and PATH

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -1,5 +1,1 @@
 #!/usr/bin/env sh
-
-test -e "${HOME}/.iterm2_shell_integration.bash" && source "${HOME}/.iterm2_shell_integration.bash"
-test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
-# test -e "$(brew --prefix)/etc/bash_completion" && source "$(brew --prefix)/etc/bash_completion"
--- macos/vars.sh
+++ macos/vars.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-export ANSIBLE_CONFIG=~/.ansible.cfg
-export BYOBU_PREFIX=/usr/local
-export PATH="/Library/Developer/CommandLineTools/usr/bin:$PATH"
+export ANSIBLE_CONFIG="$HOME/.local/ansible.cfg"
+export PATH="$PATH:/Library/Developer/CommandLineTools/usr/bin"
 export DOTFILES="$HOME/.dotfiles"
