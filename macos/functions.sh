c4485534f558b46ea43577b145636ff351032710
Rob Dumas <robdumas@gmail.com>
2025-05-30 12:03:44 -0500

    Moved .localrc in zshrc; tweaked a function
    Signed-off-by: Rob Dumas <robdumas@gmail.com>

--- macos/functions.sh
+++ macos/functions.sh
@@ -4,6 +4,6 @@
 function gpg_cache () {
     gpg-connect-agent /bye &> /dev/null # 1
     eval $(op signin) # 2
-    op get item vmgevmdnbbuui3evhksdftjhju --fields password | /usr/lib/gnupg2/gpg-preset-passphrase --preset 80160C5055DA07978E939C0575A4E8DA0B1ECF27 # 3
+    op item get $OP_GPG_PASSPHRASE --fields password | /usr/lib/gnupg2/gpg-preset-passphrase --preset $GPG_KEY_FOR_OP_TO_UNLOCK # 3
 }
 gpg_cache # 4
