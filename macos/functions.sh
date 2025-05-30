9a2102af15e5f48e9251c055fd0ff7bbaf9b5fdb
Rob Dumas <robdumas@gmail.com>
2025-05-30 12:05:00 -0500

    MINOR: fixed path
    Signed-off-by: Rob Dumas <robdumas@gmail.com>

--- macos/functions.sh
+++ macos/functions.sh
@@ -4,6 +4,6 @@
 function gpg_cache () {
     gpg-connect-agent /bye &> /dev/null # 1
     eval $(op signin) # 2
-    op item get $OP_GPG_PASSPHRASE --fields password | /usr/lib/gnupg2/gpg-preset-passphrase --preset $GPG_KEY_FOR_OP_TO_UNLOCK # 3
+    op item get $OP_GPG_PASSPHRASE --fields password | /usr/local/MacGPG2/libexec/gpg-preset-passphrase --preset $GPG_KEY_FOR_OP_TO_UNLOCK # 3
 }
 gpg_cache # 4
