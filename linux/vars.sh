d936ce5fda33e278f6e0e1fdc7a78c174183f5cc
Rob Dumas <robdumas@gmail.com>
2025-05-31 10:42:38 -0500

    Added GIT_PAGER var
    Without this, git shows ugly control characters instead of a nicely-formatted log.
    
    Signed-off-by: Rob Dumas <robdumas@gmail.com>

--- linux/vars.sh
+++ linux/vars.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
+export GIT_PAGER='less -R'
 export LESS="-Fiqs"
 export SYSTEMD_PAGER='' # Disable systemctl's automatic pager
--- macos/vars.sh
+++ macos/vars.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
 export ANSIBLE_CONFIG="$HOME/.local/ansible.cfg"
+export GIT_PAGER='more -R'
 export PATH="$PATH:/Library/Developer/CommandLineTools/usr/bin"
 export DOTFILES="$HOME/.dotfiles"
