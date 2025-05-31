bcd0b1e60f2c0bc610c6325b66badf94bb77e4b3
Rob Dumas <robdumas@gmail.com>
2025-05-31 11:00:45 -0500

    MINOR: repointing `brewup` alias to Brewfile
    Signed-off-by: Rob Dumas <robdumas@gmail.com>

--- darwin/aliases.sh
+++ darwin/aliases.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 alias alint=ansible-lint
 alias aplay=ansible-playbook
-alias brewup="brew bundle --file $DOTFILES/macos/Brewfile && brew update && brew upgrade && brew cleanup"
+alias brewup="brew bundle --file $DOTFILES/darwin/Brewfile && brew update && brew upgrade && brew cleanup"
 alias o=open
 alias oo='open .'
 alias ls='ls -H'
