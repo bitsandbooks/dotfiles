bd4215ef444ee9ece374ebeb8199b7d89b3cf9fa
Rob Dumas <robdumas@gmail.com>
2025-06-25 20:28:42 -0500

    Reusable SSH agent for shells
    Signed-off-by: Rob Dumas <robdumas@gmail.com>

--- sh/functions.sh
+++ sh/functions.sh
@@ -35,3 +35,29 @@ function httpcompression() {
 
 # Gzip-enabled `curl`
 function gurl() { curl -sH "Accept-Encoding: gzip" "$@" | gunzip }
+
+# Reusable SSH Agent
+function start_ssh_agent() {
+  local agent_info_file="$HOME/.ssh/agent.env"
+
+  # Reuse existing agent if possible
+  if [[ -f "$agent_info_file" ]]; then
+    source "$agent_info_file" > /dev/null
+
+    if [[ -n "$SSH_AGENT_PID" ]] && ps -p $SSH_AGENT_PID > /dev/null 2>&1; then
+      return 0
+    fi
+
+    # Clean up stale environment file
+    rm -f "$agent_info_file"
+    unset SSH_AGENT_PID SSH_AUTH_SOCK
+  fi
+
+  # Start new ssh-agent and save its environment
+  eval "$(ssh-agent -s -E sha256)" > /dev/null
+  echo "export SSH_AUTH_SOCK=$SSH_AUTH_SOCK" > "$agent_info_file"
+  echo "export SSH_AGENT_PID=$SSH_AGENT_PID" >> "$agent_info_file"
+  chmod 600 "$agent_info_file"
+}
+
+
